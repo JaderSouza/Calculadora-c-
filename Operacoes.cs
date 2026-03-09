253a7188a700820a760c963bac126b3089463dcb
Jader Souza <jaderbezerra.s@gmail.com>
2026-03-09 15:55:33 -0300

    Primeiro commit do projeto calculadora

---
+++ Operacoes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Projeto_Calculadora
+{
+    public class Operacoes
+    {
+        public double num1;
+        public double num2;
+        public double total;
+
+        public string adicao(double num1, double num2) 
+        {
+            
+            double total = num1 + num2;
+            return "O resultado da operação soma é: " + total;
+        }
+        public string subtracao(double num1, double num2)
+        {
+            double total = num1 - num2;
+            return "O resultado da operação de subtração é: " + total;
+        }
+        public string multiplicacao(double num1, double num2)
+        {
+            double total = num1 * num2;
+            return "O resultado da operação de multiplicação é: " + total;
+        }
+        public string Divisao(double num1, double num2)
+        {
+            if (num1 > num2)
+            {
+                double total = num1 / num2;
+                return "O resultado da operação de divisão é: " + total; ;
+            }
+            else 
+            {
+                Console.WriteLine("Não é possível efetuar divisões por 0");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Projeto_Calculadora;
+using System;
+using System.Globalization;
+
+
+namespace Projeto_calculadora
+{
+    class Calculadora
+    {
+        public static void Main(string[] args)
+        {
+            LogicaCalculadora();
+        }
+        static void LogicaCalculadora()
+        {
+            //Operacoes Ops = new Operacoes();
+            Operacoes = new Operacoes();
+            Operacoes n1 = new Operacoes();
+            Operacoes n2 = new Operacoes();
+            Console.Write("Digite o primeiro número");
+            n1.num1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite + para somar, - para subitrair, * para multiplicar e / para dividir e para sair e fechar a calculadora digite 0");
+            string operacao = Console.ReadLine();
+            Console.Write("Digite o segundo número");
+            n1.num2 = double.Parse(Console.ReadLine());
+
+            while (operacao != "0")
+            {
+                if (operacao == "+")
+                {
+                    Operacoes.adicao();
+                }
+                else if (operacao == "-")
+                {
+                    Operacoes.subtracao();
+                }
+                else if (operacao == "*")
+                {
+                    Operacoes.multiplicacao();
+                }
+                else if (operacao == "/")
+                {
+                    Operacoes.Divisao();
+                }
+                else 
+                {
+                    Console.WriteLine("Opção incorreta! Digite uma opção válida");
+                }
+
+                Console.Write("Digite o primeiro número");
+                n1.num1 = double.Parse(Console.ReadLine());
+                Console.Write("Digite + para somar, - para subitrair, * para multiplicar e / para dividir e para sair e fechar a calculadora digite 0");
+                operacao = Console.ReadLine();
+                Console.Write("Digite o segundo número");
+                n2.num2 = double.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("A calculadora será fechada...");
+        }
+    }
+}
